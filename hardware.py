2d7edce607e6ff73f2cf5b4b59ff14f51948165d
black <willbroad@localhost.localdomain>
2024-01-15 06:01:03 +0300

    fixed the threads

--- hardware.py
+++ hardware.py
@@ -10,10 +10,10 @@ class SharedData:
         self.shared_variable = {
                 "data" : {},
                 "status": False
-             }
+            }
         self.event = _thread.allocate_lock()
 
-class Thread1:
+class Comm:
     def __init__(self, shared_data):
         self.name = "Thread 1"
         self.shared_data = shared_data
@@ -22,6 +22,7 @@ class Thread1:
         self.mqtt_topic = b"test_topic"
         self.wifi_ssid = "TP-Link_0772"
         self.wifi_password = "39222480"
+        self.client = None
     
     def connect_to_wifi(self):
         wlan = network.WLAN(network.STA_IF)
@@ -32,7 +33,24 @@ class Thread1:
             while not wlan.isconnected():
                 time.sleep(1)
             print("Connected to WiFi")
-
+            
+    def connect_to_broker(self):
+        self.connect_to_wifi()
+        unique_id_bytes = machine.unique_id()
+        unique_id_hex = ''.join(['{:02X}'.format(b) for b in unique_id_bytes])
+        client_id = "esp32-" + unique_id_hex
+        self.client = MQTTClient(client_id, self.mqtt_broker, port=self.mqtt_port)
+        self.client.set_callback(self.on_message)
+        try:
+            self.client.connect()
+        except Exception as e:
+            print(f"Error connecting to MQTT broker: {e}")
+            machine.reset()
+            
+        self.client.subscribe(self.mqtt_topic)
+        print(f"Subscribed to topic: {self.mqtt_topic}")
+        return True
+        
     def publish_message(self,client, message):
         try:
             client.publish(self.mqtt_topic, json.dumps(message))
@@ -44,39 +62,24 @@ class Thread1:
         print(f"Received message on topic {topic}: {message}")
         
     def run(self):
-        self.connect_to_wifi()
-        unique_id = machine.unique_id()
-        unique_id_bytes = machine.unique_id()
-        unique_id_hex = ''.join(['{:02X}'.format(b) for b in unique_id_bytes])
-        client_id = "esp32-" + unique_id_hex
-        client = MQTTClient(client_id, self.mqtt_broker, port=self.mqtt_port)
-        client.set_callback(self.on_message)
         try:
-            client.connect()
-        except Exception as e:
-            print(f"Error connecting to MQTT broker: {e}")
-            machine.reset()
-            
-        client.subscribe(self.mqtt_topic)
-        print(f"Subscribed to topic: {self.mqtt_topic}")
-        
-        try:
-            while True:
-                client.check_msg()
-                
+            is_connected =self.connect_to_broker()
+            while is_connected:
+                self.client.check_msg()
                 if self.shared_data.shared_variable["status"] == False:
-                    self.publish_message(client, self.shared_data.shared_variable["data"])
+                    self.publish_message(self.client, self.shared_data.shared_variable["data"])
                     self.shared_data.shared_variable["status"] = True
                     
         except KeyboardInterrupt:
             pass
         finally:
-            client.disconnect()
+            self.client.disconnect()
             print("Disconnected from the broker.")
 
-class Thread2:
+
+class Main:
     def __init__(self, shared_data):
-        self.name = "Thread 2"
+        self.name = "Main"
         self.shared_data = shared_data
 
     def run(self):
@@ -85,27 +88,32 @@ class Thread2:
             with self.shared_data.event:
                 if i >= 15:
                     self.shared_data.shared_variable["status"] = False
-                    self.shared_data.shared_variable["data"] = {"name": "wilbroad"}
+                    self.shared_data.shared_variable["data"] = {
+                        "id": 0,
+                        "name": "dht",
+                        "data" : {}     
+                    }
                     i = 0
             time.sleep(1)
             i+=1
 
 
 
-
-def run_thread(obj):
-    _thread.start_new_thread(obj.run, ())
 shared_data = SharedData()
-thread1 = Thread1(shared_data)
-thread2 = Thread2(shared_data)
-run_thread(thread1)
-time.sleep(1)
-run_thread(thread2)
+communication = Comm(shared_data)
+main = Main(shared_data)
+
+_thread.start_new_thread(communication.run, ())
+time.sleep(0.5)
+_thread.start_new_thread(main.run, ())
+
 try:
     while True:
         pass
+
 except KeyboardInterrupt:
     pass
+
 finally:
     _thread.exit()
 
